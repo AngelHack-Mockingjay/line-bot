29e11e66f0e23818dfa7083c79b099a42579c235
Tony Yang <superzoro168@gmail.com>
2017-07-29 15:10:24 +0800

    Pull machanism

--- app.py
+++ app.py
@@ -51,9 +51,13 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    event_main_action = event.message.text
+    if event_main_action == '拉':
+        content = ''
+        content += event_main_action + ': pull some message'
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
 
 if __name__ == "__main__":
     app.run()
